c42ff8c27b8a487e06ae717d717e4c3b6044dd8a
Vladislav <vtinishov@gmail.com>
2024-01-11 02:24:36 +0600

    сделал main контроллер для вывода вьюшки

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\CurrencyService;
+use CurrencyApi\CurrencyApi\CurrencyApiException;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('main.index');
+    }
 }
